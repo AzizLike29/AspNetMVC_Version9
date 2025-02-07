02eb7d422fabd2cac1cf8451aebb6a1dbd9e33c1
AzizLike29 <abdulfirdaus590@gmail.com>
2025-02-07 07:33:39 +0700

    feat: added database in folder SQL

--- WebTestUsersCompany/WebTestUsersCompany/obj/Debug/net9.0/WebTestUsersCompany.AssemblyInfo.cs
+++ WebTestUsersCompany/WebTestUsersCompany/obj/Debug/net9.0/WebTestUsersCompany.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTestUsersCompany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb491ca53fdca5f8dbeea0cf8a3a1fcd820f43c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTestUsersCompany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTestUsersCompany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
